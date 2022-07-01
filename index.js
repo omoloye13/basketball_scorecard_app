93d7b06998f0d7d59b1d9a5ead82644d12aa48ae
omoloye13 <omoloyefauziyat@gmail.com>
2022-07-01 07:20:46 +0100

    created a scorecard counter

---
+++ index.js
@@ -0,0 +1,26 @@
+let homeBtn = document.getElementById("home-btn");
+let guestBtn = document.getElementById("guest-btn");
+function addHome1() {
+homeBtn.textContent = 18 + 1;
+    
+    }
+function addHome2() {
+    homeBtn.textContent = 18 + 2;
+        
+    }
+    function addHome3() {
+        homeBtn.textContent = 18 + 3;
+            
+        }
+        function addGuest1() {
+            guestBtn.textContent = 10 + 1;
+                
+            }
+        function addGuest2() {
+            guestBtn.textContent = 10 + 2;
+                
+            }
+        function addGuest3() {
+            guestBtn.textContent = 10 + 3;
+                
+            }
